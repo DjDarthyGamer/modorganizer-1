5cc8d38bf94bc8dc2cbec3d9b350d6c447babcc9
Silarn <jrim@rimpo.org>
2019-01-29 20:10:53 -0600

    Update Process Improvements * 5 minute batch auto-update of up to 10 mods   - Still able to force an update of all 'unchecked' mods   - Prioritizes mods with oldest update 'age' * Implemented 1-hour wait between update checks per mod * Fixed issues with update progress display * Only enable update filter automatically if 'force update' * Improved display of version update status in mod list   - Italic text when ready to perform update check   - Tooltip indicates when next update is available * Fixed remaining issues with update fallback check * Only trigger one update API request for duplicate sources

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -396,7 +396,7 @@ MainWindow::MainWindow(QSettings &initSettings
   connect(NexusInterface::instance(&pluginContainer), SIGNAL(nxmDownloadURLsAvailable(QString,int,int,QVariant,QVariant,int)), this, SLOT(nxmDownloadURLs(QString,int,int,QVariant,QVariant,int)));
   connect(NexusInterface::instance(&pluginContainer), SIGNAL(needLogin()), &m_OrganizerCore, SLOT(nexusApi()));
   connect(NexusInterface::instance(&pluginContainer)->getAccessManager(), SIGNAL(validateFailed(QString)), this, SLOT(validationFailed(QString)));
-  connect(NexusInterface::instance(&pluginContainer)->getAccessManager(), SIGNAL(credentialsReceived(const QString&, bool)),
+  connect(NexusInterface::instance(&pluginContainer)->getAccessManager(), SIGNAL(credentialsReceived(const QString&, bool, std::tuple<int, int, int, int>)),
           this, SLOT(updateWindowTitle(const QString&, bool)));
   connect(NexusInterface::instance(&pluginContainer)->getAccessManager(), SIGNAL(credentialsReceived(const QString&, bool, std::tuple<int, int, int, int>)),
     NexusInterface::instance(&m_PluginContainer), SLOT(setRateMax(const QString&, bool, std::tuple<int, int, int, int>)));
@@ -434,6 +434,10 @@ MainWindow::MainWindow(QSettings &initSettings
   m_SaveMetaTimer.setSingleShot(false);
   connect(&m_SaveMetaTimer, SIGNAL(timeout()), this, SLOT(saveModMetas()));
   m_SaveMetaTimer.start(5000);
+
+  m_ModUpdateTimer.setSingleShot(false);
+  connect(&m_ModUpdateTimer, SIGNAL(timeout()), this, SLOT(modUpdateCheck()));
+  m_ModUpdateTimer.start(300 * 1000);
 
   setCategoryListVisible(initSettings.value("categorylist_visible", true).toBool());
   FileDialogMemory::restore(initSettings);
@@ -487,6 +491,8 @@ MainWindow::MainWindow(QSettings &initSettings
 
   updatePluginCount();
   updateModCount();
+
+  modUpdateCheck();
 }
 
 
@@ -3992,6 +3998,14 @@ void MainWindow::checkModsForUpdates()
       m_ModsToUpdate = ModInfo::checkAllForUpdate(&m_PluginContainer, this);
     }
   }
+
+  m_ModListSortProxy->setCategoryFilter(boost::assign::list_of(CategoryFactory::CATEGORY_SPECIAL_UPDATEAVAILABLE));
+  for (int i = 0; i < ui->categoriesList->topLevelItemCount(); ++i) {
+    if (ui->categoriesList->topLevelItem(i)->data(0, Qt::UserRole) == CategoryFactory::CATEGORY_SPECIAL_UPDATEAVAILABLE) {
+      ui->categoriesList->setCurrentItem(ui->categoriesList->topLevelItem(i));
+      break;
+    }
+  }
 }
 
 void MainWindow::changeVersioningScheme() {
@@ -4393,7 +4407,7 @@ QMenu *MainWindow::modListContextMenu()
   menu->addAction(tr("Enable all visible"), this, SLOT(enableVisibleMods()));
   menu->addAction(tr("Disable all visible"), this, SLOT(disableVisibleMods()));
 
-  menu->addAction(tr("Check all for update"), this, SLOT(checkModsForUpdates()));
+  menu->addAction(tr("Force update check"), this, SLOT(checkModsForUpdates()));
 
   menu->addAction(tr("Refresh"), &m_OrganizerCore, SLOT(profileRefresh()));
 
@@ -5414,18 +5428,29 @@ void MainWindow::updateDownloadView()
 
 void MainWindow::modDetailsUpdated(bool)
 {
-  if (--m_ModsToUpdate == 0) {
+  if (m_ModsToUpdate <= 0) {
     statusBar()->hide();
-    m_ModListSortProxy->setCategoryFilter(boost::assign::list_of(CategoryFactory::CATEGORY_SPECIAL_UPDATEAVAILABLE));
-    for (int i = 0; i < ui->categoriesList->topLevelItemCount(); ++i) {
-      if (ui->categoriesList->topLevelItem(i)->data(0, Qt::UserRole) == CategoryFactory::CATEGORY_SPECIAL_UPDATEAVAILABLE) {
-        ui->categoriesList->setCurrentItem(ui->categoriesList->topLevelItem(i));
-        break;
-      }
-    }
     m_RefreshProgress->setVisible(false);
   } else {
     m_RefreshProgress->setValue(m_RefreshProgress->maximum() - m_ModsToUpdate);
+  }
+}
+
+void MainWindow::modUpdateCheck()
+{
+  if (NexusInterface::instance(&m_PluginContainer)->getAccessManager()->validated()) {
+    m_ModsToUpdate += ModInfo::autoUpdateCheck(&m_PluginContainer, this);
+    m_RefreshProgress->setRange(0, m_ModsToUpdate);
+  } else {
+    QString apiKey;
+    if (m_OrganizerCore.settings().getNexusApiKey(apiKey)) {
+      m_OrganizerCore.doAfterLogin([this]() { this->checkModsForUpdates(); });
+      NexusInterface::instance(&m_PluginContainer)->getAccessManager()->apiCheck(apiKey);
+    } else { // otherwise there will be no endorsement info
+      MessageDialog::showMessage(tr("Not logged in, endorsement information will be wrong"),
+        this, true);
+      m_ModsToUpdate += ModInfo::autoUpdateCheck(&m_PluginContainer, this);
+    }
   }
 }
 
@@ -5445,12 +5470,7 @@ void MainWindow::nxmUpdatesAvailable(QString gameName, int modID, QVariant userD
     }
   }
   std::vector<ModInfo::Ptr> modsList = ModInfo::getByModID(gameNameReal, modID);
-  // Not clear to me what this is accomplishing?
-  //if (sameNexus) {
-  //  std::vector<ModInfo::Ptr> mainInfo = ModInfo::getByModID(m_OrganizerCore.managedGame()->gameShortName(), modID);
-  //  info.reserve(info.size() + mainInfo.size());
-  //  info.insert(info.end(), mainInfo.begin(), mainInfo.end());
-  //}
+
   for (auto mod : modsList) {
     QString installedFile = mod->getInstallationFile();
     for (auto update : fileUpdates) {
@@ -5498,6 +5518,7 @@ void MainWindow::nxmUpdatesAvailable(QString gameName, int modID, QVariant userD
 
     if (foundUpdate) {
       // Just get the standard data updates for endorsements and descriptions
+      mod->setLastNexusUpdate(QDateTime::currentDateTimeUtc());
       mod->updateNXMInfo();
     } else {
       // Scrape mod data here so we can use the mod version if no file update was located
@@ -5505,29 +5526,9 @@ void MainWindow::nxmUpdatesAvailable(QString gameName, int modID, QVariant userD
     }
   }
 
-  // Old endorsement and mod info updater
-  //for
-  //  if (updateData["old_file_id"].toInt() == mod->readMeta())
-  //    (*iter)->setNewestVersion(result["version"].toString());
-  //(*iter)->setNexusDescription(result["description"].toString());
-  //if (NexusInterface::instance(&m_PluginContainer)->getAccessManager()->validated() &&
-  //  result.contains("voted_by_user") &&
-  //  Settings::instance().endorsementIntegration()) {
-  //  // don't use endorsement info if we're not logged in or if the response doesn't contain it
-  //  (*iter)->setIsEndorsed(result["voted_by_user"].toBool());
-  //}
-
-  if (m_ModsToUpdate <= 0) {
+  if (--m_ModsToUpdate <= 0) {
     statusBar()->hide();
-    m_ModListSortProxy->setCategoryFilter(boost::assign::list_of(CategoryFactory::CATEGORY_SPECIAL_UPDATEAVAILABLE));
-    for (int i = 0; i < ui->categoriesList->topLevelItemCount(); ++i) {
-      if (ui->categoriesList->topLevelItem(i)->data(0, Qt::UserRole) == CategoryFactory::CATEGORY_SPECIAL_UPDATEAVAILABLE) {
-        ui->categoriesList->setCurrentItem(ui->categoriesList->topLevelItem(i));
-        break;
-      }
-    }
-  }
-  else {
+  } else {
     m_RefreshProgress->setValue(m_RefreshProgress->maximum() - m_ModsToUpdate);
   }
 }
@@ -5557,9 +5558,8 @@ void MainWindow::nxmDescriptionAvailable(QString gameName, int modID, QVariant u
       else
         mod->setIsEndorsed(false);
     }
+    mod->saveMeta();
   }
-  disconnect(sender(), SIGNAL(nxmDescriptionAvailable(QString, int, QVariant, QVariant, int)),
-    this, SLOT(nxmDescriptionAvailable(QString, int, QVariant, QVariant, int)));
 }
 
 void MainWindow::nxmEndorsementToggled(QString, int, QVariant, QVariant resultData, int)
